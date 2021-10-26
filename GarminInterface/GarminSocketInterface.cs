d0f980b9d8dc89b146b682183c71c85cb22a07ab
tnboswell <tnbozman@gmail.com>
2021-10-26 15:06:01 +1030

    Porting https://github.com/travislang/garmin_gspro_connect to c#

---
+++ GarminInterface/GarminSocketInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarminInterface
+{
+    interface GarminSocketInterface
+    {
+        void Connect();
+        void Disconnect();
+        void SendHandshake();
+        void SendPing();
+        void SendShot();
+
+
+    }
+}
